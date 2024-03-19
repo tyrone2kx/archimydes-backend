605a09b83d7a2fc54a586fbfccd021594a86e6f9
Tyrone Onyebuagu <tyrone.onyebuagu@gmail.com>
2024-03-19 12:53:43 +0100

    feat: add frontend and serve static UI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Get, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     UserModule,
   ],
